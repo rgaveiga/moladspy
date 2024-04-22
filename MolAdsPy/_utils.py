a5883133a557cd0d1b724804169b0d0b651dd4bf
rgaveiga <roberto.veiga@ufabc.edu.br>
2024-04-22 13:08:18 -0300

    Add _utils.py with a decorator

---
+++ MolAdsPy/_utils.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+def apply_owner_rules(obj):
+    def outer_wrapper(func):
+        @wraps(func)
+        def inner_wrapper(*args,**kwargs):
+            owner=obj._belongs_to
+    
+            owner._begin_handler(obj,func,**kwargs) if owner is not None else None           
+            func(*args,**kwargs)
+            owner._end_handler(obj,func,**kwargs) if owner is not None else None
+            
+        return inner_wrapper
+    
+    return outer_wrapper
+        
+        
+
